144b65f7c827cff73f8ec23eae443fd6ff5e3c6d
Victor <victorfabregascastro@gmail.com>
2020-11-09 09:34:53 +0100

    .

--- app/src/main/java/com/example/practicagrancentre/MainActivity.java
+++ app/src/main/java/com/example/practicagrancentre/MainActivity.java
@@ -2,13 +2,63 @@ package com.example.practicagrancentre;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView negocios = findViewById(R.id.negocios);
+        ImageView cartelera = findViewById(R.id.cartelera);
+        ImageView transporte = findViewById(R.id.transporte);
+        ImageView restaurantes = findViewById(R.id.restaurantes);
+        ImageView hoteles = findViewById(R.id.hoteles);
+        ImageView eventos = findViewById(R.id.eventos);
+
+        negocios.setOnClickListener(this);
+        cartelera.setOnClickListener(this);
+        transporte.setOnClickListener(this);
+        restaurantes.setOnClickListener(this);
+        hoteles.setOnClickListener(this);
+        eventos.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        ImageView img = (ImageView) v;
+
+        switch(img.getId())
+        {
+
+            case  R.id.negocios:
+
+                break;
+            case  R.id.cartelera:
+
+                break;
+            case  R.id.transporte:
+                Intent intent = new Intent(this,Transport.class);
+                startActivity(intent);
+                break;
+            case  R.id.restaurantes:
+
+                break;
+            case  R.id.hoteles:
+
+                break;
+            case  R.id.eventos:
+
+                break;
+
+
+        }
     }
 }
---
+++ app/src/main/java/com/example/practicagrancentre/Restaurante.java
@@ -0,0 +1,14 @@
+package com.example.practicagrancentre;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Restaurante extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurante);
+    }
+}
